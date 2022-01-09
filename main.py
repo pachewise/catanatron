b75bee489ef8504345b4a95921da0cd7bb331804
Bryan Collazo <bcollazo2010@gmail.com>
2022-01-09 14:35:12 -0400

    Formatting and using chrome-driver-py

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from chromedriver_py import binary_path  # this will get you the path variable
 
 capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-capabilities["goog:loggingPrefs"] = { "performance":"ALL" }
-driver = webdriver.Chrome(desired_capabilities=capabilities)
+capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
+driver = webdriver.Chrome(
+    executable_path=binary_path, desired_capabilities=capabilities
+)
 driver.get("https://colonist.io/")
+
 
 def consume_messages():
     messages = []
     for wsData in driver.get_log("performance"):
-        # print(wsData) 
+        # print(wsData)
         wsJson = json.loads((wsData["message"]))
         if wsJson["message"]["method"] == "Network.webSocketFrameReceived":
-            message = wsJson["message"]["params"]["response"]  # ignoring timestamp, requestId
+            message = wsJson["message"]["params"][
+                "response"
+            ]  # ignoring timestamp, requestId
             data = parse_websocket_message(message["payloadData"])
 
-            print("RECEIEVED", find_in_enum(int(data['id']), ReceivedWSType))
+            print("RECEIEVED", find_in_enum(int(data["id"]), ReceivedWSType))
             print(data)
             messages.append(data)
         if wsJson["message"]["method"] == "Network.webSocketFrameSent":
@@ -33,29 +39,34 @@ def consume_messages():
             messages.append(data)
     return messages
 
+
 play_bots_btn = driver.find_element(By.ID, "landingpage_cta_playvsbots")
 play_bots_btn.click()
 print("CLICKED PLAY WITH BOTS")
-while(True):
+while True:
     messages = consume_messages()
+    driver.implicitly_wait(10)  # seconds
     canvas = driver.find_element(By.CSS_SELECTOR, "canvas")
     # i = 0
-    clicks = [(0,0), (10,0), (20, 0), (30, 0) ]
+    clicks = [(0, 0), (10, 0), (20, 0), (30, 0)]
     for i in range(0, 100, 5):
         (x, y) = (i, 0 - i)
         print(i, "Clicking on", x, y)
         # driver.action.move_to(canvas, x, y).perform()
         # driver.click.perform()
         # driver.action
-        drawing = ActionChains(driver)\
-            .move_to_element_with_offset(canvas, x, y)\
-            .click()\
-            .release().perform()
+        drawing = (
+            ActionChains(driver)
+            .move_to_element_with_offset(canvas, x, y)
+            .click()
+            .release()
+            .perform()
+        )
         # drawing.perform()
         # i += 1
         # x -= 5
         # y -= 5
-        
+
     print(len(messages))
     time.sleep(10)  # wait for game to load up
     # driver.implicitly_wait(10) # seconds
@@ -66,7 +77,7 @@ while(True):
 #     background: red;
 #     z-index: 1000000000;
 #     position: fixed;
-#     top: 160px; vs 730px. -570 to go down to actions bar. 
+#     top: 160px; vs 730px. -570 to go down to actions bar.
 #     left: 165px; vs 715px. -550 to go to end turn
 # import asyncio
 # from pyppeteer import launch
